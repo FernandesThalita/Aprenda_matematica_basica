7e6920e16d2763a61ca3e06e4cbf432d29bd1231
Thalita <119790076+FernandesThalita@users.noreply.github.com>
2023-09-02 14:26:28 -0300

    Add files via upload

---
+++ js/reveal.js
@@ -0,0 +1,9 @@
+//configuração padrao que os docs pedem 
+window.sr = ScrollReveal({reset: true});
+
+
+sr.reveal('.container_resumo img',{ 
+  duration: 1000,
+  origin: 'left',
+  distance: '30px',
+});
