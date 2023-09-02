589a049b7fbc2417feb9a237e8605cf417003e74
FernandesThalita <thalitahfernades@gmail.com>
2023-09-02 17:01:15 -0300

    editando lading page

--- js/reveal.js
+++ js/reveal.js
@@ -1,9 +1,45 @@
 //configuração padrao que os docs pedem 
 window.sr = ScrollReveal({reset: true});
 
-
-sr.reveal('.container_resumo img',{ 
+// >>> Pag index
+sr.reveal('.section_resumo ',{ 
   duration: 1000,
   origin: 'left',
-  distance: '30px',
+  distance: '30px'
 });
+sr.reveal('.section_resumo .descricao h2 ',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '10px',
+  delay: 400
+});
+sr.reveal('.section_resumo .descricao .box-btn ',{ 
+  duration: 1000,
+  origin: 'bottom',
+  distance: '10px',
+  delay: 500
+});
+
+
+sr.reveal('.section_about_us ',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '30px',
+  delay: 500
+});
+sr.reveal('.section_about_us .titulo ',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '30px',
+  delay: 600,
+});
+sr.reveal('.section_about_us .box-descricao button ',{ 
+  duration: 1000,
+  origin: 'bottom',
+  distance: '30px',
+  delay: 600,
+});
+
+
+
+///////////// FIM pag index /////
